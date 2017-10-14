e21e01709264742836faa24c31f175460228f680
Broda Noel <brodanoel@gmail.com>
2017-10-13 22:19:51 -0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Home from 'pages/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        The app
+        <Home />
       </div>
     );
   }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        Home!!!
+      </div>
+    );
+  }
+}
+
+export default Home;
