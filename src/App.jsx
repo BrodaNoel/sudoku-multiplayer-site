09488b96c3178e84bc470bae0fb20ed544986d49
Broda Noel <brodanoel@gmail.com>
2017-10-16 23:03:48 -0300

    Implement Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import './App.css';
 
@@ -6,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Route exact path='/' component={ Home } />
+        </Switch>
       </div>
     );
   }
