049323c86766ca0602e47fe7f3fc942f2418e15e
Broda Noel <brodanoel@gmail.com>
2017-10-29 00:46:22 -0300

    Add COnfig

---
+++ src/functionals/Config/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+class Config extends Component {
+  render() {
+    return (
+      <div className="Config">
+        <div>
+          <input type="checkbox" disabled="disabled" /> Check for errors
+        </div>
+
+        <div>
+          <input type="checkbox" disabled="disabled" /> Check for duplicated numbers
+        </div>
+
+        <div>
+          <input type="checkbox" disabled="disabled" /> Highlight areas
+        </div>
+
+        <div>
+          <input type="checkbox" disabled="disabled" /> Highlight identical numbers
+        </div>
+
+        <div>
+          <input type="checkbox" disabled="disabled" /> Auto remove pencil notes
+        </div>
+
+        <div
+          className="button"
+          onClick={() => this.props.onSelect({ someConfig: true }) }>Save Config</div>
+      </div>
+    );
+  }
+}
+
+Config.propTypes = {
+  onSelect: PropTypes.func.isRequired
+}
+
+export default Config;
