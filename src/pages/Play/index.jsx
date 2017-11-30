78e57361181e9ed534fe0934d57585d6f989130e
Broda Noel <brodanoel@gmail.com>
2017-11-30 20:24:44 -0300

    Improve basic Play functions

--- src/pages/Play/index.jsx
+++ src/pages/Play/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import actions from 'actions';
 import Sudoku from 'functionals/Sudoku';
+import Loading from 'visuals/Loading';
+import Timer from 'visuals/Timer';
 import './styles.css';
 
 class Play extends Component {
   state = {
     type: 'alone',
-    config: {},
+    config: {
+      showTimer: true
+    },
     init: {
       0: 1,
       12: 4
@@ -13,21 +18,50 @@ class Play extends Component {
     done: {
       1: 9,
       46: 8
-    }
+    },
+
+    // TODO: Get this from backend
+    teamId: 0,
+
+    startAt: Date.now(),
+
+    isLoading: true
   };
 
-  onChange = (index, prevNumber, newNumber) => {
-    //
+  constructor(props) {
+    super(props);
+
+    const gameId = this.props.match.params.gameId;
+
+    actions.game.get(gameId).then((game) => {
+      this.setState({
+        type: game.type,
+        config: game.config,
+        startAt: game.startAt,
+        isLoading: false
+      });
+    });
+  }
+
+  onChange = (i, newValue) => {
+    const gameId = this.props.match.params.gameId;
+    actions.game.changeValue(gameId, this.state.teamId, i, newValue);
   }
 
   render() {
     return (
       <div className="Play">
-        <Sudoku
-          init={this.state.init}
-          config={this.state.config}
-          done={this.state.done}
-          onChange={this.onChange} />
+        { !this.state.isLoading && this.state.config.showTimer ? <Timer startAt={this.state.startAt} /> : null }
+
+        {
+          this.state.isLoading
+            ? <Loading />
+            : <Sudoku
+                init={this.state.init}
+                config={this.state.config}
+                done={this.state.done}
+                onChange={this.onChange} />
+        }
       </div>
     );
   }
