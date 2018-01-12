ec26e2d243756c420c00c9e0b20e59d1f35981a8
Broda Noel <brodanoel@gmail.com>
2018-01-12 01:44:51 -0300

    Add "FinishAt" prop in Timer visual

--- src/visuals/Timer/index.jsx
+++ src/visuals/Timer/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import utils from 'utils';
 import './styles.css';
 
@@ -8,12 +7,32 @@ class Timer extends Component {
     time: '00:00:00'
   };
 
+  constructor(props) {
+    super(props);
+
+    if (props.finishAt) {
+      this.state.time = utils.timeFormat.secondsToTime((props.finishAt - this.props.startAt) / 1000);
+    }
+  }
+
   componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        time: utils.timeFormat.secondsToTime((Date.now() - this.props.startAt) / 1000)
-      })
-    }, 1000);
+    if (!this.props.finishAt) {
+      this.interval = setInterval(() => {
+        this.setState({
+          time: utils.timeFormat.secondsToTime((Date.now() - this.props.startAt) / 1000)
+        })
+      }, 1000);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.finishAt) {
+      clearInterval(this.interval);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
@@ -21,10 +40,6 @@ class Timer extends Component {
       <div className="Timer">{this.state.time}</div>
     );
   }
-}
-
-Timer.propTypes = {
-  startAt: PropTypes.number.isRequired
 }
 
 export default Timer;
